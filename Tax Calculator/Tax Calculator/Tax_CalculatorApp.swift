cf0543127983c622915d4f8148f040c1332fbdc6
Sarah Clark <sarahireneclark@gmail.com>
2023-11-06 07:44:20 -0600

    Initial commit of project

---
+++ Tax Calculator/Tax Calculator/Tax_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tax_CalculatorApp.swift
+//  Tax Calculator
+//
+//  Created by Sarah Clark on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Tax_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
