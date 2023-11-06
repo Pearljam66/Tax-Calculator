6a778e4e84acb2fe1abe20ba56480e0b80ebcb18
Sarah Clark <sarahireneclark@gmail.com>
2023-11-06 10:47:08 -0600

    ResultsView wireframe implemented

--- Tax Calculator/Tax Calculator/Views/ResultsView.swift
+++ Tax Calculator/Tax Calculator/Views/ResultsView.swift
@@ -12,9 +12,38 @@ struct ResultsView: View {
     var taxBreakdown: [Double] = [5, 10, 15]
 
     var body: some View {
-        PieChart()
-            .data(taxBreakdown)
-            .chartStyle(ChartStyle(backgroundColor: .white, foregroundColor: ColorGradient(.blue, .purple)))
+        VStack {
+            Text("Summary")
+                .font(.system(size: 36))
+                .fontWeight(.bold)
+
+            PieChart()
+                .data(taxBreakdown)
+                .chartStyle(ChartStyle(backgroundColor: .white, foregroundColor: ColorGradient(.blue, .purple)))
+
+                Text("Before Tax:")
+                    .font(.system(size: 32))
+                    .padding(.vertical)
+                Text("$100,000")
+                    .font(.system(size: 32))
+                    .padding(.vertical)
+                Text("After Tax:")
+                    .font(.system(size: 32))
+                    .padding(.vertical)
+                Text("$75,000")
+                    .font(.system(size: 32))
+                    .padding(.vertical)
+
+                Text("Post Tax Salary")
+                ProgressView("", value: 20, total: 100)
+
+                Text("Tax")
+                ProgressView("", value: 20, total: 100)
+
+                Text("Social Security Tax")
+                ProgressView("", value: 20, total: 100)
+        }
+        .padding()
     }
 }
 
