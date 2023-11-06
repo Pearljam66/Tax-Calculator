064353f243186ff2c20120a5b4086e15a2210306
Sarah Clark <sarahireneclark@gmail.com>
2023-11-06 09:56:10 -0600

    Updated chart code: - Removed PieChartView and it's corresponding code. - Added SwiftUICharts package dependency. - Added basic pie chart functionality to ResultsView.

--- Tax Calculator/Tax Calculator/Views/ResultsView.swift
+++ Tax Calculator/Tax Calculator/Views/ResultsView.swift
@@ -5,27 +5,16 @@
 //  Created by Sarah Clark on 11/6/23.
 //
 
+import SwiftUICharts
 import SwiftUI
 
 struct ResultsView: View {
     var taxBreakdown: [Double] = [5, 10, 15]
 
     var body: some View {
-        let taxData: [(Double, Color)] = taxBreakdown.enumerated().map { index, value in
-            return (value, colorForValue(Double(index)))
-        }
-
-        PieChartView(data: taxData)
-    }
-
-    func colorForValue(_ index: Double) -> Color {
-        if index == 0 {
-            return .gray
-        } else if index == 1 {
-            return .blue
-        } else {
-            return .pink
-        }
+        PieChart()
+            .data(taxBreakdown)
+            .chartStyle(ChartStyle(backgroundColor: .white, foregroundColor: ColorGradient(.blue, .purple)))
     }
 }
 
