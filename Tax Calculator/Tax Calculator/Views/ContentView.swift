e00fa16e5a08979eaddc7336044b124ac4c8f17f
Sarah Clark <sarahireneclark@gmail.com>
2023-11-07 09:28:34 -0600

    Added calculation code and additional ResultsView UI

--- Tax Calculator/Tax Calculator/Views/ContentView.swift
+++ Tax Calculator/Tax Calculator/Views/ContentView.swift
@@ -9,27 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var salary: String = ""
+    @State private var isSalaryValid: Bool = false
     @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
-        VStack {
-            Text("Annual Salary")
-                .padding(.bottom, 75)
+        NavigationView {
+            VStack {
+                Text("Annual Salary")
+                    .padding(.bottom, 75)
 
-            TextField("", text: $salary)
-                .frame(width: 200)
-                .border(borderColor, width: 1)
-                .padding(.bottom, 75)
-                .keyboardType(.numberPad)
+                TextField("", text: $salary)
+                    .frame(width: 200)
+                    .border(borderColor, width: 1)
+                    .padding(.bottom, 75)
+                    .keyboardType(.numberPad)
 
-            Button {}
-            label: {
-                Text("Calculate Tax")
+                NavigationLink(destination: ResultsView(salary:$salary), isActive: $isSalaryValid, label: {
+                    Text("Calculate Tax")
+                        .bold()
+                        .frame(width: 200, height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(10)
+                        .onTapGesture {
+                            goToResultsView()
+                        }
+                })
             }
-            .buttonStyle(.borderedProminent)
+            .padding()
+            .navigationTitle("Main Page")
         }
-        .padding()
+
     }
+
+        func goToResultsView() {
+            if (Float(salary) != nil) {
+                if (Float(salary)! > 0) {
+                    isSalaryValid = true
+                }
+            }
+        }
 
     var borderColor: Color {
         return colorScheme == .dark ? Color.white : Color.black
--- Tax Calculator/Tax Calculator/Views/ResultsView.swift
+++ Tax Calculator/Tax Calculator/Views/ResultsView.swift
@@ -5,48 +5,116 @@
 //  Created by Sarah Clark on 11/6/23.
 //
 
-import SwiftUICharts
 import SwiftUI
+import SwiftUICharts
 
 struct ResultsView: View {
     var taxBreakdown: [Double] = [5, 10, 15]
+    @Binding var salary: String
 
     var body: some View {
-        VStack {
-            Text("Summary")
-                .font(.system(size: 36))
-                .fontWeight(.bold)
+        let salaryNumber = Double(salary)!
+        var incomeTax: Double = 0
+        var socialSecurityTax: Double = 0
 
-            PieChart()
-                .data(taxBreakdown)
-                .chartStyle(ChartStyle(backgroundColor: .white, foregroundColor: ColorGradient(.blue, .purple)))
+        if (salaryNumber > 11000) {
+            if (salaryNumber > 44725) {
+                if (salaryNumber > 182100) {
+                    incomeTax += (44725 - 11001) * 0.1
+                    incomeTax += (182100 - 44726) * 0.12
+                    incomeTax += (salaryNumber - 182100) * 0.24
+                } else {
+                    incomeTax += (44725 - 11001) * 0.1
+                    incomeTax += (salaryNumber - 44725) * 0.12
+                }
+            } else {
+                incomeTax += (salaryNumber - 11000) * 0.2
+            }
+        }
+        socialSecurityTax = salaryNumber * 0.0765
+        let postTaxSalary = salaryNumber - incomeTax - socialSecurityTax
+
+        let salaryString = String(format:"$%.2F", salaryNumber)
+        let postTaxSalaryString = String(format: "$%.2F", postTaxSalary)
+        let incomeTaxString = String(format: "$%.2F", incomeTax)
+        let socialSecurityTaxString = String(format: "$%.2F", socialSecurityTax)
+
+        let taxBreakdown: [Double] = [postTaxSalary, incomeTax, socialSecurityTax]
+
+       return NavigationView {
+            VStack {
+
+                PieChart()
+                    .data(taxBreakdown)
+                    .chartStyle(ChartStyle(backgroundColor: .white, foregroundColor: ColorGradient(.blue, .purple)))
 
                 Text("Before Tax:")
-                    .font(.system(size: 32))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.system(size: 20))
+                    .fontWeight(.bold)
+
+                Text(salaryString)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.system(size: 25))
                     .padding(.vertical)
-                Text("$100,000")
-                    .font(.system(size: 32))
-                    .padding(.vertical)
+                Divider()
+                    .background(
+                        Rectangle()
+                            .fill(Color.gray)
+                            .frame(height: 3)
+                        )
                 Text("After Tax:")
-                    .font(.system(size: 32))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.system(size: 20))
+                    .fontWeight(.bold)
+
+                Text(postTaxSalaryString)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.system(size: 25))
                     .padding(.vertical)
-                Text("$75,000")
-                    .font(.system(size: 32))
-                    .padding(.vertical)
+                Divider()
+                    .background(
+                        Rectangle()
+                            .fill(Color.gray)
+                            .frame(height: 3)
+                    )
 
                 Text("Post Tax Salary")
-                ProgressView("", value: 20, total: 100)
+                    .fontDesign(.serif)
+                    .fontWeight(.heavy)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding()
+                ProgressView(postTaxSalaryString, value: postTaxSalary / salaryNumber * 100, total: 100)
+                    .padding(10)
+                    .border(.gray, width: 3)
+                    .cornerRadius(4)
 
                 Text("Tax")
-                ProgressView("", value: 20, total: 100)
+                    .fontDesign(.serif)
+                    .fontWeight(.heavy)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding()
+                ProgressView(incomeTaxString, value: incomeTax / salaryNumber * 100, total: 100)
+                    .padding(10)
+                    .border(.gray, width: 3)
+                    .cornerRadius(4)
 
                 Text("Social Security Tax")
-                ProgressView("", value: 20, total: 100)
+                    .fontDesign(.serif)
+                    .fontWeight(.heavy)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding()
+                ProgressView(socialSecurityTaxString, value: 7.65, total: 100)
+                    .padding(10)
+                    .border(.gray, width: 3)
+                    .cornerRadius(4)
+            }
         }
-        .padding()
+       .padding()
+       .navigationTitle("Summary")
     }
 }
 
 #Preview {
-    ResultsView()
+    ResultsView(salary: .constant("100"))
 }
